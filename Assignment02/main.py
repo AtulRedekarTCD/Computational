051510a32d9cd51bc78106f924278d888152a7e9
AtulRedekarTCD <redekara@tcd.ie>
2022-11-23 09:28:54 +0000

    Assignment 02 Files

---
+++ Assignment02/main.py
@@ -0,0 +1,71 @@
+import numpy as np
+from scipy import signal as sc
+
+# @ Computational Assignment 02 Median filter
+#   This code illustrates to implement 1-D median filter
+#   from input signal datawith the window.
+#
+# @ File Name  : main.py
+# @ Author     : Atul Redekar
+# @ Supervisior: Angeliki Katsenou
+# @ Date       : 22/11/22
+
+
+# Definition
+# function Name :  median_filter(data, window)
+# Inputs        :  list data   -> Input signal Array
+#               :  int  window -> Size of Window
+# Output        :  list median_array  -> Median Computed Array
+
+def median_filter(data, window):
+
+    # Check for odd window size
+    if(window % 2 == 1):
+        padded_data = data
+        length = len(data)
+        median_array = []
+        padding = (window - 1) // 2  # converting
+        print("padding: ", padding)
+
+    #   Appending '0' padding at start and end of the signal data
+        for i in range(padding * 2):
+            if (i < padding):
+                padded_data.insert(0, 0)
+            else:
+                padded_data.insert(len(padded_data), 0)
+
+        print("padded_data : ", padded_data)
+
+        for index in range(length):
+            # extracting window size arrays
+            window_array = padded_data[index: window + index]
+            window_array.sort()       # Sorting in window size arrays in ascending order
+
+            # printing window size arrays
+            print("window_array", index, " : ", window_array)
+
+            # storing median values
+            median = window_array[int((window - 1) / 2)]
+
+            median_array.append(median)  # creating median values array
+
+        print("\nmedian_array : ", median_array)
+        print("\n")
+
+        return median_array
+    else:
+        # Throwing error for even window
+        print("Window size has to be odd number")
+
+
+if __name__ == "__main__":
+
+    data = [1, 2, 3, 6, 10, 7, 2, 1]
+    window = 3
+
+    filter_out = sc.medfilt(data,kernel_size = window) # Using medfilt from scipy
+    
+    print(" medfilt : ", filter_out) 
+
+# calling median_filter to computer Median
+    median_filter(data, window)
