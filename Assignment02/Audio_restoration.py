2bbb84c51b005dfc83ed47730267584ec5b9a2c7
AtulRedekarTCD <redekara@tcd.ie>
2022-11-28 13:52:43 +0000

    Added Comment and DocStrings

--- Assignment02/Audio_restoration.py
+++ Assignment02/Audio_restoration.py
@@ -6,20 +6,83 @@ import matplotlib.pyplot as plt
 import median_filter as median
 from scipy.io.wavfile import write
 
-samplerate, audio_data = wavfile.read("apple_audio.wav")
+def get_filter_len(degraded_data_1d):
 
+    '''
+    get_filter_len give the best filter length for degraded audio data 
+    by computing minimum MSE.
+
+    Returns the filter length.
+
+            Inputs:
+                    degraded_data_1d   (array)  : degraded audio data
+
+            Output:
+                    filter_len         (int)    : filter length 
+    '''
+
+    print(" type" , type(detect))
+    print(" max " , np.max(detect))
+
+    
+
+    # Computing MSE for the window size from 3 to 19
+    windows = [3,5,7,9,11,13,15,17,19]
+
+    MSE = np.zeros(len(windows))
+
+    Threshold = 32000
+
+    for w in range(len(windows)):
+        
+        window = windows[w]	
+        padding = (window - 1) // 2
+
+        for index in range(len(degraded_data_1d)):
+            if( abs(degraded_data_1d[index]) >= Threshold ):            
+                res_data[index] = median.median_filter(degraded_data_1d[index - padding : index + padding + 1], window)
+
+        MSE[w] =  np.square(np.subtract(audio_data_1d,res_data)).mean()
+
+        # print(MSE[w]," : ",window)
+    
+    
+    # Returns a minimum index MSE
+    min_index = np.where(MSE == MSE.min())
+
+    # Convert it into integer value
+    min_i = int(min_index[0])
+    
+    # Store the minimum filter length
+    filter_len = windows[min_i] 
+
+    plt.plot(windows,MSE,'-r*')
+    plt.xlim(1,20,1)
+    plt.ylim(0.4,1.8)
+    plt.xlabel("Filter Length")
+    plt.ylabel("MSE")
+    plt.title("MSE vs Filter Size")
+    plt.show()
+
+    return filter_len
+
+# reading Data from Audio files
+samplerate, audio_data = wavfile.read("apple_audio.wav")
 samplerate, degraded_data = wavfile.read("degraded.wav")
 
+# Using single channel 
 audio_data_1d = audio_data[:, 1]
-
 degraded_data_1d = degraded_data[:, 1]
 
+# Creating a Array of detect for position of clicks
 detect = np.zeros(len(degraded_data_1d))
 
+Threshold = 32000
 for i in range(len(degraded_data_1d)):
-    if( abs(degraded_data_1d[i]) >= 32000):
+    if( abs(degraded_data_1d[i]) >= Threshold ):
         detect[i] = 1
 
+# Plotting original, degraded and detected clicks
 figure, axs = plt.subplots(3, 1)
 axs[0].plot(audio_data_1d)
 axs[0].set_title('Original Audio Data')
@@ -33,55 +96,26 @@ for ax in axs.flat:
 
 plt.show()
 
-# For Normalized Values
-# Normalized_data = (degraded_data_1d / degraded_data_1d.max() )
-# print(Normalized_data)
 
-# plt.figure(figsize=(14, 8), dpi=80)
-# plt.plot(Normalized_data)
-# plt.show()
-
-
-		
-# plt.figure(figsize=(14, 8), dpi=80)
-# plt.plot(detect)
-# plt.show()
-
+# Creating a copy of degraded data
 res_data = degraded_data_1d
 
 degraded_data_1d = degraded_data_1d.tolist()
 
-windows = [3,5,7,9,11,13,15,17,19]
-MSE = np.zeros(len(windows))
+# Computing best filter length
+window = get_filter_len(degraded_data_1d)
 
-for w in range(len(windows)):
-    
-    window = windows[w]	
-    padding = (window - 1) // 2
+# print(" Best filter len : ",window)
 
-    for index in range(len(degraded_data_1d)):
-        if( abs(degraded_data_1d[index]) >= 32000):
-            res_data[index] = median.median_filter(degraded_data_1d[index - padding : index + padding + 1], window)
-
-    MSE[w] =  np.square(np.subtract(audio_data_1d,res_data)).mean()
-    # print(MSE[w]," : ",window)
-
-plt.plot(windows,MSE,'-r*')
-plt.xlim(1,20,1)
-plt.ylim(0.4,1.8)
-plt.xlabel("Filter Length")
-plt.ylabel("MSE")
-plt.title("MSE vs Filter Size")
-plt.show()
-
-# Best Filter LE gth from the Data
-window = 11
+# data point required before and after click to find median of data
+padding = (window - 1) // 2
 
 for index in range(len(degraded_data_1d)):
-        if( abs(degraded_data_1d[index]) >= 32000):
+        if( detect[index] == 1 ):
             res_data[index] = median.median_filter(degraded_data_1d[index - padding : index + padding + 1], window)
 
 
+# Plotting original, degraded and restored audio data
 figure, axs = plt.subplots(3, 1)
 axs[0].plot(audio_data_1d)
 axs[0].set_title('Original Audio Data')
@@ -95,5 +129,7 @@ for ax in axs.flat:
 
 plt.show()
 
+# Writing the restored data to create a output audio file
 res_data1 = np.array(res_data)
 write("output.wav", samplerate, res_data1.astype(np.int16))
+
--- Assignment02/median_filter.py
+++ Assignment02/median_filter.py
@@ -2,26 +2,32 @@ import numpy as np
 
 def median_filter(data, window):
 
-#     print(type(data))
+    '''
+    median_filter sorts the input Audio data array and 
+    Returns the median from Audio data array.
+
+            Inputs:
+                    data   (int)  : Audio data array contains the clicks
+                    window (int)  : filter length
+
+            Output:
+                    median (int)  : median of array
+    '''
+
     # Check for odd window size
     if(window % 2 == 1):
         length = len(data)
-        # print("data: ", data)
         window_array = [0] * len(data)
-        padding = (window - 1) // 2  # converting
-#         print("padding: ", padding)
+        padding = (window - 1) // 2
 
-
-
-        # extracting window size arrays
-#         window_array = data[index: window + index]
-        window_array = np.sort(data)       # Sorting in window size arrays in ascending order
+        # Sorting in window size arrays in ascending order
+        window_array = np.sort(data)       
 
         # printing window size arrays
         # print("window_array ", window_array)
 
         # storing median values
-        median = window_array[int((window - 1) / 2)]
+        median = window_array[padding]
 
         # print("median",  median)
 
